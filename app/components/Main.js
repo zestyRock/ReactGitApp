839f39efb349a7eaed90624bc22fb79797d68546
ZestyRock <yanguo1208@gmail.com>
2017-04-18 17:55:00 -0700

    Init first commit

---
+++ app/components/Main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function(){
+    return (
+      <div>
+        hello world!
+      </div>
+    );
+  }
+});
+
+// ReactDOM.render(<Main />, document.getElementById('app'));
+module.exports = Main;
